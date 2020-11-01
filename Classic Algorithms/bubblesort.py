64affbe275ecfee7b40ff2093daf46574a5f4a3d
snayeri <68785737+snayeri@users.noreply.github.com>
2020-11-01 01:54:21 -0700

    Update bubblesort.py

--- Classic Algorithms/bubblesort.py
+++ Classic Algorithms/bubblesort.py
@@ -1,24 +1,13 @@
 def bubble(lst):
     l = len(lst)
-    for i in xrange(l):
-        for j in xrange(l-1,i, -1):
+    for i in range(l):
+        ## not adjacent elements according to wikipedia
+        for j in range(l-1,i, -1):
             if lst[i] > lst[j]:
                 lst[i], lst[j] = lst[j], lst[i]
     return lst
 
-if __name__ == "__main__":
-    print "please input integer number array"
-    lst = []
-    while 1:
-        try:
-            n = raw_input(">")
-        except:
-            print ""
-            break
-        lst.extend([int(i) for i in n.split()])
-    print "origin: ", lst
-    print "sorted: ", bubble(lst)
- ####### My method: 
+####### My method: 
  def bubble_merge(lst):
     #start with a non-zero number
     swap_count = 1
@@ -34,3 +23,19 @@ if __name__ == "__main__":
                 count +=1
         swap_count = count
     return lst
+
+
+
+if __name__ == "__main__":
+    print "please input integer number array"
+    lst = []
+    while 1:
+        try:
+            n = raw_input(">")
+        except:
+            print ""
+            break
+        lst.extend([int(i) for i in n.split()])
+    print "origin: ", lst
+    print "sorted: ", bubble(lst)
+ 
