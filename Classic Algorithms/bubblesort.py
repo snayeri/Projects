44083958ed6e3381cbea0fc1121a5bbd4992fa99
snayeri <68785737+snayeri@users.noreply.github.com>
2020-11-01 01:35:35 -0700

    Update bubblesort.py

--- Classic Algorithms/bubblesort.py
+++ Classic Algorithms/bubblesort.py
@@ -18,3 +18,19 @@ if __name__ == "__main__":
         lst.extend([int(i) for i in n.split()])
     print "origin: ", lst
     print "sorted: ", bubble(lst)
+ ####### My method: 
+ def bubble_merge(lst):
+    #start with a non-zero number
+    swap_count = 1
+    #continue while swaps are done
+    while swap_count !=0:
+        count = 0
+        #choose two adjacent elements iterating from 0 until 1 before last element
+        for r in range(len(lst)-1):
+            elm0,elm1 = lst[r], lst[r+1]        
+            #swap them in order if necessary
+            if elm1 < elm0:
+                lst[r], lst[r+1] = elm1, elm0
+                count +=1
+        swap_count = count
+    return lst
